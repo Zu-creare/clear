36b6070499fbb71dc8e64ae3f24db5ea4718be02
magomed <magomed.zubayrayev@list.ru>
2021-03-04 23:18:37 +0300

    initial commit

---
+++ ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by M on 13.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Another Print statement")
+        
+        
+    }
+
+
+}
+
